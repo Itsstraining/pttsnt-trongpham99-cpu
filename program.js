058040cbaa6ce36641165627c5d46ff158b92dc4
unknown <trong.phamtranduc@gmail.com>
2021-01-28 10:10:51 +0700

    lan 1

--- program.js
+++ program.js
@@ -1,8 +1,28 @@
-// Add new functions, variables here
-
 function main(input) {
-  // Your code goes here
-  // Using console.log to answer the question
+  let inp = input.split(' ');
+  let n = parseInt(inp[0]);
+  
+  for (let i = 2; i <= n; i++) 
+  {
+      while( isPrime(i) && (n % i == 0) ) 
+      {
+          console.log(i);
+          n = n / i;
+      }
+  }
+  return 0;
 }
+
+function isPrime(n) 
+{
+    let m = (Math.sqrt(n));
+    for (let i = 2; i <= m; i++) 
+    {
+        if(n % i == 0) 
+            return 0;
+    }
+    return 1;
+}
+
 
 module.exports = main;
